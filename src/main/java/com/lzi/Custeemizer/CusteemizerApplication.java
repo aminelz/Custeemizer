894226fdd6a36d3e0452556c91b87454d95af73e
Amine Laazizi <amine.laazizi@gmail.com>
2019-08-22 11:12:04 +0100

    Added Initial Files

---
+++ src/main/java/com/lzi/Custeemizer/CusteemizerApplication.java
@@ -0,0 +1,13 @@
+package com.lzi.Custeemizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CusteemizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CusteemizerApplication.class, args);
+	}
+
+}

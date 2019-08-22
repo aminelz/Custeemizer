465f9564c0ac3709df1bb3076b87824df8895db9
Amine Laazizi <amine.laazizi@gmail.com>
2019-08-22 12:58:13 +0100

    Added H2 Database dependency

--- src/main/java/com/lzi/Custeemizer/CusteemizerApplication.java
+++ src/main/java/com/lzi/Custeemizer/CusteemizerApplication.java
@@ -2,12 +2,18 @@ package com.lzi.Custeemizer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class CusteemizerApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(CusteemizerApplication.class);
+
 	public static void main(String[] args) {
+
 		SpringApplication.run(CusteemizerApplication.class, args);
+		logger.info("Hello to my application");
 	}
 
 }

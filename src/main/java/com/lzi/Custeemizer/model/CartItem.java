548c6511dae259b92e4cd22fb0faa8c76d84ba03
Amine Laazizi <amine.laazizi@gmail.com>
2019-08-29 11:40:14 +0100

    Add CartItem Entity

---
+++ src/main/java/com/lzi/Custeemizer/model/CartItem.java
@@ -0,0 +1,33 @@
+package com.lzi.Custeemizer.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="cartitem")
+public class CartItem {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long item_id;
+
+    private int quantity;
+    private double total_price;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getTotal_price() {
+        return total_price;
+    }
+
+    public void setTotal_price(double total_price) {
+        this.total_price = total_price;
+    }
+}

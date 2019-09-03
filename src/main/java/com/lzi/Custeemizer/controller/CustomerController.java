f25dbf3522eb8a76cb5aee9f0e638c32c1a093a8
Amine Laazizi <amine.laazizi@gmail.com>
2019-09-03 16:47:46 +0100

    Added new queries to the user repository

--- src/main/java/com/lzi/Custeemizer/controller/CustomerController.java
+++ src/main/java/com/lzi/Custeemizer/controller/CustomerController.java
@@ -1,11 +1,16 @@
 package com.lzi.Custeemizer.controller;
 
+import com.lzi.Custeemizer.model.EndUser;
 import com.lzi.Custeemizer.model.UserCustomer;
 import com.lzi.Custeemizer.repository.CustomerRepository;
 import com.lzi.Custeemizer.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CustomerController {
@@ -15,10 +20,31 @@ public class CustomerController {
     @Autowired
     private UserRepository userrepo;
 
-    @RequestMapping("/listcustomers")
-    public Iterable<UserCustomer> getCustomers(){
-        return custrepo.findAll();
+    @RequestMapping("/listofcustomers")
+    public List<EndUser> getCustomers(){
+        return userrepo.findbyAdmin(false);
     }
+
+    @RequestMapping("/listofadmins")
+    public List<EndUser> getAdmins(){
+        return userrepo.findbyAdmin(true);
+    }
+
+    @RequestMapping("/fulllistofcustomers")
+    public List<Object[]> getAllCustomers(){
+        return userrepo.findAllCustomers();
+    }
+
+    @RequestMapping("/userofcustomer/{id}")
+    public List<Object[]> getUserofCustomer(@PathVariable(value="id") Long id){
+        return userrepo.findUserByCustomer(id);
+    }
+
+//    public Iterable<UserCustomer> getCustomers(){
+//        return custrepo.findAll();
+//    }
+
+//    @RequestMapping("/listadmins")
 
 
 }
--- src/main/java/com/lzi/Custeemizer/repository/UserRepository.java
+++ src/main/java/com/lzi/Custeemizer/repository/UserRepository.java
@@ -2,9 +2,40 @@ package com.lzi.Custeemizer.repository;
 
 
 import com.lzi.Custeemizer.model.EndUser;
+import com.lzi.Custeemizer.model.UserCustomer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
+//<EndUser,Long> Defines that the CRUD repo is for the EndUser Class and that the ID field is of type Long
 public interface UserRepository extends CrudRepository <EndUser,Long> {
+
+    //Finding Admin/Normal Customers One query is is native sql and other is in jpql
+//    @Query("select x from EndUser x where x.admin = ?1 ") //jpql query
+//    List<EndUser> findAdmin(Boolean admin);
+    @Query(value= "SELECT * FROM enduser WHERE admin = ?1", nativeQuery=true) //sql query
+    public List<EndUser> findbyAdmin(Boolean admin);
+
+    // /!\ /!\/!\ /!\/!\ /!\/!\ /!\/!\ /!\/!\ /!\/!\ /!\/!\ /!\/!\ /!\/!\ /!\/!\ /!\
+    //  /!\ /!\/!\ /!\ /!\ /!\ NEED TO USE NAMED QUERIES /!\ /!\ /!\ /!\/!\ /!\/!\
+    // /!\ /!\/!\ /!\/!\ /!\/!\ /!\/!\ /!\/!\ /!\/!\ /!\/!\ /!\/!\ /!\/!\ /!\/!\ /!\
+    
+    //query to get all non-sensitive information of all customers
+    @Query(value="SELECT u.first_name, u.last_name, u.email, c.birth_date, c.phone_number," +
+            "s.street, s.city, s.zipcode, s.country," +
+            "p.cc_type, p.cc_number FROM enduser u JOIN customer c ON u.user_ID = c.user_ID " +
+            "JOIN shipping s ON  c.customer_ID = s.customer_ID " +
+            "JOIN payment p ON c.customer_ID = p.customer_ID",nativeQuery = true)
+    public List<Object[]> findAllCustomers();
+
+    //query that looks for the user matching the customer_ID provided in the url and displays their important joined table fields
+    @Query(value="SELECT u.last_name, u.first_name, u.email, c.birth_date, c.phone_number FROM customer c " +
+            "JOIN enduser u ON u.user_ID = c.user_ID WHERE c.customer_ID = ?1  ", nativeQuery = true)
+    public List<Object[]> findUserByCustomer(long customer_ID);
+
 
 
 }
